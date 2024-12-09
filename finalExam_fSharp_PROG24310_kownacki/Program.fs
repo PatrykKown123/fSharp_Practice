b7241364c67e171661cfe31dd60125e49ab66df7
pattycake1337 <159066586+pattycake1337@users.noreply.github.com>
2024-12-09 14:00:42 -0500

    Implemented a recursive function to find the greatest number first. It will be used to be passed as a starting value for finding the smallest number

---
+++ finalExam_fSharp_PROG24310_kownacki/Program.fs
@@ -0,0 +1,14 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+let list = [2;3;4;1;5;6;-1;8;9]
+let rec greatestNumber list = 
+    let rec greatestNumber' list greatestNumber =
+        match list with
+        | [] -> greatestNumber
+        | x::xs when x > greatestNumber -> greatestNumber' xs x
+        | _::xs -> greatestNumber' xs greatestNumber
+    greatestNumber' list 0
+
+let smallestNumber list =
+    
+    
