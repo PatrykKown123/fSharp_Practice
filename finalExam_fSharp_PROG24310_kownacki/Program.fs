b80761332f2bd43572feb52072fff26e422951b0
pattycake1337 <159066586+pattycake1337@users.noreply.github.com>
2024-12-09 15:25:49 -0500

    Added comments

--- finalExam_fSharp_PROG24310_kownacki/Program.fs
+++ finalExam_fSharp_PROG24310_kownacki/Program.fs
@@ -1,20 +1,29 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 
 let list = [2;3;4;1;-10;5;6;-1;8;9]
-let rec greatestNumber list = 
+
+// greatestNumber retrieves the greatest number from the list
+let rec greatestNumber list =
+    // inner recursive function will return the greatest number
     let rec greatestNumber' list greatestNumber =
         match list with
+        // when list is empty - return the greatest number
         | [] -> greatestNumber
+        // when list head is greater than current greatestNumber - reassign greatestNumber
         | x::xs when x > greatestNumber -> greatestNumber' xs x
+        // all other cases - pass the current greatest number
         | _::xs -> greatestNumber' xs greatestNumber
+    // self call - beginning the check at 0
     greatestNumber' list 0
 
+// works same as above but for small number
 let smallestNumber list =
     let rec smallestNumber' list smallestNum =
         match list with
         | [] -> smallestNum
         | x::xs when x < smallestNum -> smallestNumber' xs x
         | _::xs -> smallestNumber' xs smallestNum
+        // passes a function to begin count from the greatest number
     smallestNumber' list (greatestNumber list)
     
 printf "The smallest number of list is: %d" (smallestNumber list)
