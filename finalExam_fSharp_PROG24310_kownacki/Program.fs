093178c2c87538272065e8ca567bf204662f9d01
pattycake1337 <159066586+pattycake1337@users.noreply.github.com>
2024-12-09 15:22:36 -0500

    Double checking to see if code works - and it does!

--- finalExam_fSharp_PROG24310_kownacki/Program.fs
+++ finalExam_fSharp_PROG24310_kownacki/Program.fs
@@ -1,6 +1,6 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 
-let list = [2;3;4;1;5;6;-1;8;9]
+let list = [2;3;4;1;-10;5;6;-1;8;9]
 let rec greatestNumber list = 
     let rec greatestNumber' list greatestNumber =
         match list with
--- finalExam_fSharp_PROG24310_kownacki/obj/Debug/net9.0/finalExam_fSharp_PROG24310_kownacki.AssemblyInfo.fs
+++ finalExam_fSharp_PROG24310_kownacki/obj/Debug/net9.0/finalExam_fSharp_PROG24310_kownacki.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("finalExam_fSharp_PROG24310_kownacki")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7241364c67e171661cfe31dd60125e49ab66df7")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+520ce2784d72eb5973f33c20beb75d7c5e757d80")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("finalExam_fSharp_PROG24310_kownacki")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("finalExam_fSharp_PROG24310_kownacki")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
